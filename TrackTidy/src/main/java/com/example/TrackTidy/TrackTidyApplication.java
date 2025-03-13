efadd744d55f2a22e84612461fe0887597687d7e
M_Farsith <mohammed.farsith@gmail.com>
2025-03-13 21:26:06 +0530

    Initial Commit

---
+++ TrackTidy/src/main/java/com/example/TrackTidy/TrackTidyApplication.java
@@ -0,0 +1,14 @@
+package com.example.TrackTidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackTidyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TrackTidyApplication.class, args);
+	}
+
+}
---
+++ TrackTidy/src/test/java/com/example/TrackTidy/TrackTidyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrackTidy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackTidyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
