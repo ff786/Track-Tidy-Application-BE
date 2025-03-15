1645882a040e702dd452a8a66feb38fbd5b9ee4a
M_Farsith <mohammed.farsith@gmail.com>
2025-03-15 10:37:34 +0530

    Initial Commit

---
+++ src/test/java/com/example/Track_Tidy_BE/TrackTidyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Track_Tidy_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackTidyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
